a3a7254790aec7b79aba6dbfc70ec21256cadfe5
ariet <ariet.alisherov@gmail.com>
2026-05-31 23:31:37 -0600

    Fixed some errors in logo.tsx and add container component

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -5,13 +5,24 @@ import './logo.css'
 
 async function Logo() {
   const webData = await getWebsiteInfo()
+  const logo = webData?.logo
+
+  const isMediaObject = logo && typeof logo === 'object'
+
+  if (!isMediaObject || !logo.url) {
+    return (
+      <Link className="logo-link" href="/">
+        <span className="logo-text">Logo</span>
+      </Link>
+    )
+  }
 
   return (
     <Link className="logo-link" href="/">
       <Image
         className="header-logo"
         alt={webData.logo?.alt || 'logo'}
-        src={webData.logo?.url || '/fallback.png'}
+        src={webData.logo?.url}
         width={220}
         height={108}
       />
