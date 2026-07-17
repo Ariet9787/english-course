8a54bacaa7cad849d49ebbd4ffefc3a68694de93
ariet <ariet.alisherov@gmail.com>
2026-07-17 20:59:09 +0600

    Add company intro and styles

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,8 @@
+import CompanyIntroSection from '@/components/home/company-intro-section'
 import { getWebsiteInfo } from '@/lib/apiServices'
 
 export default async function HomePage() {
   const webInfo = await getWebsiteInfo()
-  return <></>
+
+  return <CompanyIntroSection company={webInfo} />
 }
